e371fff8dba587aa2fa981bd540af8a96e69a1e0
CMD <gautamkailashsingh@gmail.com>
2020-10-07 22:18:17 +0530

    Add project files.

---
+++ URP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            patternMatch obj = new patternMatch();
+            Console.WriteLine("Enter first name");
+            while (true)
+            {
+                string fName = Console.ReadLine();
+                bool checkFName = obj.matchFName(fName);
+                if (!checkFName)
+                    Console.WriteLine("You have entered wrong first name. Enter again");
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ URP/patternMatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace URP
+{
+    class patternMatch
+    {
+        string patternFName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public bool matchFName(string fName)
+        {
+            return Regex.IsMatch(fName, patternFName);
+        }
+    }
+}
