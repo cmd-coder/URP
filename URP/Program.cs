0da0007c81dc36a13a24a55fa4bc98fee2216675
CMD <gautamkailashsingh@gmail.com>
2020-10-07 22:43:09 +0530

    [Gautam] Added a feature to validate email id of the user

--- URP/Program.cs
+++ URP/Program.cs
@@ -8,11 +8,12 @@ namespace URP
         {
             Console.WriteLine("Welcome to User Registration Program");
             patternMatch obj = new patternMatch();
+            string fName, lName, email;
             Console.WriteLine("Enter first name");
 
             while (true)
             {
-                string fName = Console.ReadLine();
+                fName = Console.ReadLine();
                 bool checkFName = obj.matchFName(fName);
                 if (!checkFName)
                     Console.WriteLine("You have entered wrong first name. Enter again");
@@ -24,10 +25,22 @@ namespace URP
 
             while (true)
             {
-                string fName = Console.ReadLine();
-                bool checkFName = obj.matchFName(fName);
-                if (!checkFName)
+                lName = Console.ReadLine();
+                bool checkLName = obj.matchFName(lName);
+                if (!checkLName)
                     Console.WriteLine("You have entered wrong last name. Enter again");
+                else
+                    break;
+            }
+
+            Console.WriteLine("Enter email");
+
+            while (true)
+            {
+                email = Console.ReadLine();
+                bool checkEmail = obj.matchEmail(email);
+                if (!checkEmail)
+                    Console.WriteLine("You have entered wrong email id. Enter again");
                 else
                     break;
             }
--- URP/patternMatch.cs
+++ URP/patternMatch.cs
@@ -8,10 +8,16 @@ namespace URP
     class patternMatch
     {
         string patternFName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        string patternEmail = "^[a-zA-Z0-9]+([._+-][a-z0-9A-Z]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-A]{2})?$";
 
         public bool matchFName(string fName)
         {
             return Regex.IsMatch(fName, patternFName);
+        }
+
+        public bool matchEmail(string email)
+        {
+            return Regex.IsMatch(email, patternEmail);
         }
     }
 }
